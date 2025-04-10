c8986ea57646738cba3ee89847c61e1d75c23f14
Guillermo Pérez Bendadda <gperez-b@cbr2s2.42barcelona.com>
2025-04-10 16:55:27 +0200

    cambio en part, invite mode y who en archivos separados

---
+++ src/commands/Invite.cpp
@@ -0,0 +1,39 @@
+#include "../../inc/Server.hpp"
+#include "../../inc/NumericReplies.hpp"
+
+#include <iostream>  // Para salida por consola
+#include <string>
+#include <sstream> 
+
+void Server::handleInvite(Client *client, std::istringstream &iss) {
+		std::string targetNick, channelName;
+		if (!(iss >> targetNick >> channelName)) {
+			sendReplyTo(client, ERR_NEEDMOREPARAMS, "INVITE", "Not enough parameters");
+			return;
+		}
+		Client *target = findClientByNick(targetNick);
+		if (!target) {
+			sendReplyTo(client, ERR_NOSUCHNICK, targetNick, "No such nick");
+			return;
+		}
+		Channel *channel = getChannelByName(channelName);
+		if (!channel) {
+			sendReplyTo(client, ERR_NOSUCHCHANNEL, channelName, "No such channel");
+			return;
+		}
+		if (!channel->isInviteOnly()) {
+			sendReplyTo(client, ERR_CHANNELISFULL, channelName, "Cannot join channel (+l)");
+			return;
+		}
+		if (channel->isInvited(target)) {
+			sendReplyTo(client, ERR_USERONCHANNEL, targetNick + " " + channelName, "User is already on the channel");
+			return;
+		}
+		channel->addInvited(target);
+		std::string reply341 = ":server 341 " + client->getNickname() + " " + targetNick + " " + channelName + "\r\n";
+		send(client->getFd(), reply341.c_str(), reply341.size(), 0);
+		std::string inviteMsg = ":" + client->getPrefix() + " INVITE " + targetNick + " :" + channelName + "\r\n";
+		send(target->getFd(), inviteMsg.c_str(), inviteMsg.size(), 0);
+
+		std::cout << "Client " << client->getFd() << " invited user " << target->getFd() << " to channel " << channelName << std::endl;
+	}
---
+++ src/commands/Who.cpp
@@ -0,0 +1,40 @@
+#include "../../inc/Server.hpp"
+#include "../../inc/NumericReplies.hpp"
+
+#include <iostream>  // Para salida por consola
+#include <string>
+#include <sstream> 
+
+void Server::handleWho(Client *client, std::istringstream &iss) {
+		std::string channelname;
+		if (!(iss >> channelname)) {
+			sendReplyTo(client, ERR_NEEDMOREPARAMS, "WHO", "Not enough parameters");
+			return;
+		}
+		Channel *channel = getChannelByName(channelname);
+		if (!channel) {
+			sendReplyTo(client, ERR_NOSUCHCHANNEL, channelname, "No such channel");
+			return;
+		}
+		if (!channel->hasClient(client)) {
+			sendReplyTo(client, ERR_NOTONCHANNEL, channelname, "You're not on that channel");
+			return;
+		}
+		const std::vector<Client *> &clients = channel->getClients();
+		for (size_t i = 0; i < clients.size(); i++) {
+			Client *target = clients[i];
+			std::string status;
+			if (clients[i]->isAway())
+				status = "G";
+			else
+				status = "H";
+			if (channel->isOperator(clients[i]))
+				status += "@";
+			std::string whoReply = ":server 352 " + client->getNickname() + " " +
+			channelname + " " + target->getUsername() + " host server " +
+			target->getNickname() + " " + status + " :0 " + target->getUsername() + "\r\n";
+			send(client->getFd(), whoReply.c_str(), whoReply.size(), 0);
+		}
+		std::string endMsg = ":server 315 " + client->getNickname() + " " + channelname + " :End of /WHO list\r\n";
+		send(client->getFd(), endMsg.c_str(), endMsg.size(), 0);
+	}
