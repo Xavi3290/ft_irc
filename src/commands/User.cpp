8af88fccd8bcba505b1d5ccbe3f45fa3c5384ca1
MaxMaltas <maltas.max@gmail.com>
2025-04-24 17:39:05 +0200

    Realname parsing done

--- src/commands/User.cpp
+++ src/commands/User.cpp
@@ -3,7 +3,8 @@
 
 #include <iostream>  // Para salida por consola
 #include <string>
-#include <sstream> 
+#include <sstream>
+#include <cstdio>  // EOF
 
 bool isValidUsername(const std::string &username) {
     for (size_t i = 0; i < username.size(); ++i) {
@@ -15,13 +16,13 @@ bool isValidUsername(const std::string &username) {
 
 void Server::handleUser(Client *client, std::istringstream &iss)
 {
-    std::string username, userMode, hostname, realname;
+    std::string username, userMode, hostname, realname, token, rest;
 
     if (client->isRegistered()) {
         sendReplyTo(client, ERR_ALREADYREGISTRED, "", "You may not reregister");
         return;
     }
-    if (!(iss >> username) || !(iss >> userMode) || !(iss >> hostname) || !(iss >> realname)) {
+    if (!(iss >> username) || !(iss >> userMode) || !(iss >> hostname) || !(iss >> token) || (token == ":" && !(iss >> rest))) {
         sendReplyTo(client, ERR_NEEDMOREPARAMS, "USER", "Not enough parameters");
         return;
     }
@@ -29,9 +30,22 @@ void Server::handleUser(Client *client, std::istringstream &iss)
         client->send(username + ": Invalid username try with alphanumeric and '-' or '_'");
         return;
     }
+
     client->setUsername(username);
-    client->setRealname(realname.substr(1));
-    std::cout << "Client " << client->getFd() << " set username to " << username << std::endl;
+    
+    if (token[0] == ':') {
+        realname = token.substr(1);
+        if (realname.size() > 0)
+            realname += " " + rest;
+        else
+            realname += rest;
+        while (iss >> rest)
+            realname += " " + rest;
+    } else {
+        realname = token;
+    }
+    client->setRealname(realname);
+    std::cout << "Client " << client->getFd() << " set username to " << username << "& realname: " << realname << std::endl;
     if (!client->getNickname().empty() && !client->getUsername().empty() && client->hasProvidedPass() && !client->isRegistered()) {
         sendReplyTo(client, RPL_WELCOME, "", ("Welcome to the 42 IRC Server Network " + client->getPrefix()));
         client->setRegistered(true);
